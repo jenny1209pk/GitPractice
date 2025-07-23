6595655bf38caf18bb8038850453ed81375878bd
Jenny <jennyluv1209@gmail.com>
2025-07-23 11:12:16 -0700

    changed the text again!

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My favorite color is blue!")
+            Text("I love to eat pasta!")
         }
         .padding()
     }
